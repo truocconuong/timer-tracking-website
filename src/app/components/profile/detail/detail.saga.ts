aae486d00ada3aa33354a95a03730eaea83e9c5e
Dat <tiendat.phung11@gmail.com>
2020-08-26 22:10:28 +0700

    dev/app-desktop

--- src/app/components/profile/detail/detail.saga.ts
+++ src/app/components/profile/detail/detail.saga.ts
@@ -17,7 +17,7 @@ import { Router } from '@angular/router';
 import { profileRouter } from '../profile.const';
 
 function* watchProfileDetailFormRendered() {
-  yield takeLatest(RENDER_PROFILE_DETAIL_FORM, function*(action) {
+  yield takeLatest(RENDER_PROFILE_DETAIL_FORM, function* (action) {
     yield put({ type: FETCH_PROFILE_DETAIL_REQUESTED });
   });
 }
@@ -25,7 +25,7 @@ function* watchProfileDetailFormRendered() {
 function* fetchProfileDetail(action) {
   const api = AppInjector.get(ApiService);
   try {
-    let result = yield api.admin.user.profile().toPromise();
+    let result = yield api.auth.profile().toPromise();
     yield put({ type: FETCH_PROFILE_DETAIL_SUCCESSED, data: result });
   } catch (e) {
     yield put({ type: API_CALL_ERROR, error: e });
