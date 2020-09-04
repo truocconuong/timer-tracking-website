fa6ded835aa4f47da51bd095ef54a0cc6f5a4d9c
Manh Chu <chuhuumanh331997@gmail.com>
2020-09-05 02:55:10 +0700

    worktime

--- src/app/components/admin/user/work_times/work_times.component.ts
+++ src/app/components/admin/user/work_times/work_times.component.ts
@@ -5,6 +5,9 @@ import * as _ from 'lodash';
 import { BaseComponent } from '../../../base.component';
 import { removeUserRequested } from '../edit/edit.actions';
 import { FETCH_ALL_WORK_TIMES_REQUESTED } from './work_times.actions';
+import { ExportService } from './../../../../../app/services/export.services';
+import * as moment from 'moment';
+import { work_times } from './work_times.reducer';
 
 @Component({
   selector: 'app-list',
@@ -31,8 +34,12 @@ export class WorkTimesComponent extends BaseComponent implements OnInit {
       value: 3
     }
   ];
-  constructor(private route: Router) {
+  public exportService;
+  public start;
+  public end;
+  constructor(private route: Router, exportSer: ExportService) {
     super();
+    this.exportService = exportSer;
     this.navigationSubscription = this.route.events.subscribe((e: any) => {
       if (e instanceof NavigationEnd) {
         this.dispatch({
@@ -46,33 +53,75 @@ export class WorkTimesComponent extends BaseComponent implements OnInit {
     this.init();
   }
 
-  changeUserStatus(item) {
-  }
+  changeUserStatus(item) {}
 
   removeUser(item) {
     this.dispatch(removeUserRequested({ id: item.id }));
   }
 
-  geneareteFirstCheckin(work_times){
-    const data = _.orderBy(work_times,['id'],['ASC']);
+  geneareteFirstCheckin(work_times) {
+    const data = _.orderBy(work_times, ['id'], ['ASC']);
     const getFirstData = _.first(data);
     return getFirstData.checkin;
-
   }
 
- geneareteLastCheckout(work_times){
-  const data = _.orderBy(work_times,['id'],['ASC']);
-  const getLastData = _.last(data);
-  return getLastData.checkout;
+  geneareteLastCheckout(work_times) {
+    const data = _.orderBy(work_times, ['id'], ['ASC']);
+    const getLastData = _.last(data);
+    return getLastData.checkout;
   }
 
+  exportExceldata = (users) => {
+    const dateChuan = Number('9:00'.replace(':', ''));
+    const dataExport = [];
+    const muon = 'Muộn';
+    const dungGio = 'Đúng giờ';
+    const khongDiemDanh = 'Chưa checkin';
+    const now = moment();
+    for (const user of users) {
+      const worktimes = user.work_times;
+      if (_.isEmpty(worktimes)) {
+        const data = {
+          email: user.email,
+          date : moment().format('DD/MM/YYYY'),
+          type: khongDiemDanh
+        };
+        dataExport.push(data);
+      } else {
+        const check = _.filter(worktimes, (worktime) => moment(worktime.checkin).format('DD/MM/YYYY') === moment().format('DD/MM/YYYY'));
+        if (_.isEmpty(check)) {
+          const data = {
+            email: user.email,
+            date : moment().format('DD/MM/YYYY'),
+            type: khongDiemDanh
+          };
+          dataExport.push(data);
+        } else {
+          const first = _.orderBy(check, ['id'], ['ASC'])[0];
+          let hours: any = moment(first.checkin).format('HH:mm');
+          hours = Number(hours.replace(':', ''));
+          let data: any = {
+            email: user.email,
+            date : moment().format('DD/MM/YYYY'),
+            time: moment(first.checkin).format('HH:mm')
+          };
+          if (hours > dateChuan) {
+            data.type = muon;
+          } else {
+            data.type = dungGio;
+          }
+          dataExport.push(data);
+        }
+      }
+    }
+    this.exportService.exportExcel(dataExport, 'export');
+  };
 
   mapStateToProps(state) {
     return {
       payload: state.Admin.User.work_times
     };
   }
-
   mapDispatchToProps(dispatch) {
     return {
       dispatch
