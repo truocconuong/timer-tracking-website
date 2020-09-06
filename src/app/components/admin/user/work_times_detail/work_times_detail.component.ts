bde5048306b5a1e21bca7d08b62f019ca2ba1074
Manh Chu <chuhuumanh331997@gmail.com>
2020-09-06 11:26:05 +0700

    check here

--- src/app/components/admin/user/work_times_detail/work_times_detail.component.ts
+++ src/app/components/admin/user/work_times_detail/work_times_detail.component.ts
@@ -71,6 +71,39 @@ export class WorkTimesDetailComponent extends BaseComponent implements OnInit {
   }
 
 
+
+  diffSessionInSec(session) {
+    return Math.floor(((new Date(session.checkout) as any) - (new Date(session.checkin) as any)) / 1000);
+  }
+
+  formatedSessionDiffTime(session) {
+    return this.stringToHHMMSS(this.diffSessionInSec(session));
+  }
+
+  total(sessions) {
+    const diff = _.sumBy(sessions, (item) => this.diffSessionInSec(item));
+    return this.stringToHHMMSS(diff);
+  }
+
+  stringToHHMMSS = function (str: any) {
+    const sec_num = parseInt(str, 10);
+    let hours: any = Math.floor(sec_num / 3600);
+    let minutes: any = Math.floor((sec_num - hours * 3600) / 60);
+    let seconds: any = sec_num - hours * 3600 - minutes * 60;
+
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    return hours + ':' + minutes + ':' + seconds;
+  };
+
+
   mapStateToProps(state) {
     return {
       payload: state.Admin.User.work_times_detail
